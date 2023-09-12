16fb66ae6d5d4c91070b1bd8151c5e77f1caad61
VARDHANVASISTA <139366474+VARDHANVASISTA@users.noreply.github.com>
2023-09-12 16:45:32 +0530

    Update bmi_calculator.py

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,1 +1,6 @@
-#
+print("Welcome to BMI Calculator!!!")
+height = input("Enter your height in metres: ")
+weight = input("Enter your weight in kg: ")
+bmi = float(weight)/float(height)**2
+b1 = int(bmi)
+print("Your BMI is: " + bmi + "\n approx.: " + b1)
