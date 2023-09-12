ef3b6905d780e44489ecc26cb4f24bb5899c92d6
VARDHANVASISTA <139366474+VARDHANVASISTA@users.noreply.github.com>
2023-09-12 22:45:27 +0530

    Create time_left_to_live.py

---
+++ time_left_to_live.py
@@ -0,0 +1,7 @@
+age = input("What is your current age? ")
+ag = int(age)
+yl = 90 - ag
+dl = yl * 365
+ml = yl * 12
+wl = yl * 52
+print(f"You have {dl} days or {wl} weeks or {ml} months left if you live till 90 years of age")
