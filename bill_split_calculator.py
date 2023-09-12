8eb97bb15ce527037bf6614a4a06c4e9d08e3b52
VARDHANVASISTA <139366474+VARDHANVASISTA@users.noreply.github.com>
2023-09-12 23:07:52 +0530

    Update bill_split_calculator.py

--- bill_split_calculator.py
+++ bill_split_calculator.py
@@ -10,3 +10,18 @@ tb += tip
 split = tb / np
 sp = round(split , 2)
 print(f"Each person should pay Rs. {sp}")
+
+#Below one is more accurate to two decimal places
+print("Welcome to the bill split calculator!!")
+total = input("What was the total bill? Rs. ")
+tb = float(total)
+per = input("What percentage tip would you like to give? 10 or 12 or 15? ")
+pt = int(per)
+peo = input("How many people are splitting the bill? ")
+np = int(peo)
+tip = pt / 100 * tb
+tb += tip
+split = tb / np
+sp = "{: .2f}".format(split)
+print(f"Each person should pay Rs. {sp}")
+
