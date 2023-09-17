9c8b60c5472b9912b40cf50a8815848537350253
VARDHANVASISTA <139366474+VARDHANVASISTA@users.noreply.github.com>
2023-09-17 23:09:23 +0530

    Update lottery.py

--- lottery.py
+++ lottery.py
@@ -1,3 +1,4 @@
+#Basic version
 import random
 print("!!WELCOME to LOTTERY systems!!")
 names_string = input("Enter everybody's names separated by a comma and a space. \n>>")
@@ -5,3 +6,27 @@ names = names_string.split(", ")
 ch = random.randint(0,(len(names)-1))
 n = names[ch].upper()
 print("Congratulations... "+n+"\nYou have WON the LOTTERY!!")
+
+#shortened version of the above
+import random
+print("!!WELCOME to LOTTERY systems!!")
+names_string = input("Enter everybody's names separated by a comma and a space. \n>>")
+names = names_string.split(", ")
+print("Congratulations... "+names[random.randint(0,(len(names)-1))].upper()+"\nYou have WON the LOTTERY!!")
+
+
+
+#Below one is an easier version
+import random
+print("!!WELCOME to LOTTERY systems!!")
+names_string = input("Enter everybody's names separated by a comma and a space. \n>>")
+names = names_string.split(", ")
+n = random.choice(names).upper()
+print("Congratulations... "+n+"\nYou have WON the LOTTERY!!")
+
+#shortened version of the above
+import random
+print("!!WELCOME to LOTTERY systems!!")
+names_string = input("Enter everybody's names separated by a comma and a space. \n>>")
+names = names_string.split(", ")
+print("Congratulations... "+(random.choice(names)).upper()+"\nYou have WON the LOTTERY!!")
