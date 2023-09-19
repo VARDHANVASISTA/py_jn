8cd5357865934a52c3b1877c400ee4f66a5a57af
VARDHANVASISTA <139366474+VARDHANVASISTA@users.noreply.github.com>
2023-09-19 22:49:52 +0530

    Update matrix_marker.py

--- matrix_marker.py
+++ matrix_marker.py
@@ -8,6 +8,8 @@ print("This is a 3X3 matrix you have row number and column number as 1,2,3 for b
 print("To see a particular position type its row number and its column number as a continuous number")
 position = input("Which position do you want to see? ")
 c = int(position) % 10
+#or c = int(position[1])
 r = int(int(position) / 10)
+#or r = int(position[1])
 map[r - 1][c - 1] = "X"
 print(f"{row1}\n{row2}\n{row3}")
