05af7ed0ee813d49a3b784ef8c33ed2f26f5d24c
VARDHANVASISTA <139366474+VARDHANVASISTA@users.noreply.github.com>
2023-10-04 21:07:46 +0530

    Update caesar-cipher_known_key.py

--- caesar-cipher_known_key.py
+++ caesar-cipher_known_key.py
@@ -67,10 +67,31 @@ def decryption():
         else:
             orm += pc
     print(f"Decrypted message is...\n{orm}")
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88 
+ 
+           88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
 sm = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 spch = ['!','@','#','$','^','&','*','(',')','_','','+','=','[',']','{','}','|',':',';',',','/','?','<','>','₹','%','"']
 cap = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 num = ['1','2','3','4','5','6','7','8','9','0']
+print(logo)
 run = True
 while(run):
     ch = input("Choices are...\n 1. Encrypt\n 2. Decrypt\n Enter any other key to exit\nEnter your choice:\n")
@@ -80,3 +101,4 @@ while(run):
         decryption()
     else:
         run = False
+        print("Thank you...")
